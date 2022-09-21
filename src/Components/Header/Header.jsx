6d5148bc3bbd3745aea8ea390ab6610aef692a78
thangdachoang <thang.dachoang@gmail.com>
2022-09-21 19:26:56 +0700

    fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div className='text-center py-4gi bg-dark text-white'>TRY GLASSES APP ONLINE</div>
+      <div className='text-center py-4 bg-dark text-white'>TRY GLASSES APP ONLINE</div>
     )
   }
 }
